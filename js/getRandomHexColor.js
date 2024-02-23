3928ff21fbea5158ac89e67e0e443109fcacb72a
lassandra85 <lassandra85@gmail.com>
2024-02-23 15:11:58 +0100

    getRandomHexColorJs

--- js/getRandomHexColor.js
+++ js/getRandomHexColor.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+/* function getRandomHexColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+} */
